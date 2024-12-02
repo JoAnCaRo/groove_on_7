8ed1d34cd1643eccf60a271e9484f454c3a7b2b7
Jose <jause6@duck.com>
2024-12-02 19:33:14 +0100

    Añadiendo efectos con Javascript(nav|smootScroll)

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// Importa funciones y estilos necesarios
+import { smoothScroll } from './utils/animations.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM completamente cargado y procesado');
+  smoothScroll();
+});
