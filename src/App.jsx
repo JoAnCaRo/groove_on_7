37099bb69c67b562df6e3fe85c56be5f34ece837
Jose <jause6@duck.com>
2024-12-09 18:39:54 +0100

    WEB DEPLOYED 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import Navbar from './components/Navbar';
 import HeaderTitle from './components/HeaderTitle';
 import Hero from './components/Hero';
@@ -40,4 +40,48 @@ const App = () => {
   );
 };
 
+export default App; */
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeaderTitle from './components/HeaderTitle';
+import Hero from './components/Hero';
+import About from './components/About';
+import LiveSessions from './components/LiveSessions';
+import Playlists from './components/Playlists';
+import CreatePlaylist from './components/CreatePlaylist';
+import Events from './components/Events';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { ScrollProvider } from './context/ScrollContext';
+
+import './styles/globals.css';
+import './styles/navbar.css';
+import './styles/hero.css';
+import './styles/about.css';
+import './styles/contact.css';
+import './styles/createlist.css';
+import './styles/events.css';
+import './styles/footer.css';
+import './styles/live-sessions.css';
+import './styles/playlists.css';
+
+const App = () => {
+  return (
+    <ScrollProvider>
+      <Navbar />
+      <HeaderTitle />
+      <Hero />
+      <About />
+      <LiveSessions />
+      <Playlists />
+      <CreatePlaylist />
+      <Events />
+      <Contact />
+      <Footer />
+    </ScrollProvider>
+  );
+};
+
 export default App;
+
