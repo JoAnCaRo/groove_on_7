202c97145e801aef9ae3aca859717c2476da3c2b
Jose <jause6@duck.com>
2024-12-11 16:45:41 +0100

    Seccion eventos totalmente funcional

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -10,15 +10,15 @@ const Playlists = () => {
       <p>Discover my curated playlists across styles like Disco, Funk, House, and more!</p>
 
       <div className="mixcloud-embed">
-        <iframe width="100%" height="120" src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&hide_artwork=1&feed=%2FJauseJones%2Fdisco-3%2F" frameborder="0"></iframe>
+        <iframe width="100%" height="120" src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&hide_artwork=1&feed=%2FJauseJones%2Fdisco-3%2F"></iframe>
       </div>
 
-      <div class="mixcloud-embed">
-        <iframe width="100%" height="120" src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&hide_artwork=1&feed=%2FJauseJones%2Fshake-it-malafakas%2F" frameborder="0"></iframe>
+      <div className="mixcloud-embed">
+        <iframe width="100%" height="120" src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&hide_artwork=1&feed=%2FJauseJones%2Fshake-it-malafakas%2F"></iframe>
       </div>
 
-      <div class="mixcloud-embed">
-        <iframe width="100%" height="120" src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&hide_artwork=1&feed=%2FJauseJones%2Ffunkywankenoby%2F" frameborder="0"></iframe>
+      <div className="mixcloud-embed">
+        <iframe width="100%" height="120" src="https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&hide_artwork=1&feed=%2FJauseJones%2Ffunkywankenoby%2F"></iframe>
       </div>
 
       <div className="see-all-container">
