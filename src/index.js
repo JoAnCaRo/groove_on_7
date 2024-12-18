d3ed93979f3ff726f99c1efea4efdef6005d5953
Jose <jause6@duck.com>
2024-12-18 07:50:20 +0100

    DEPURACIÓN error event handler en DOM | index.js 2

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-// Forzar todos los event listeners a ser pasivos
-const supportsPassive = (() => {
-  let supported = false;
-  try {
-    window.addEventListener(
-      'test',
-      null,
-      Object.defineProperty({}, 'passive', {
-        get: () => (supported = true),
-      })
-    );
-  } catch (e) {}
-  return supported;
-})();
-
-if (supportsPassive) {
-  document.addEventListener('touchstart', () => {}, { passive: true });
-  document.addEventListener('touchmove', () => {}, { passive: true });
-}
-
-// Importaciones de React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Componente principal
+import App from './App';
 
-// Montar la aplicación React
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
