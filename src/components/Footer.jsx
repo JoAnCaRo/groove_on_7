656c2feb2345f85ad3ff36b2a3b4afabf5386d96
Jose <jause6@duck.com>
2024-12-18 15:26:16 +0100

    DEPLOYED OK | Comentarios Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import PrivacyPolicyPopup from './PrivacyPolicyPopup';
 
+/* Componente Footer que muestra el pie de página y maneja la ventana emergente de política de privacidad */
 const Footer = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false); // Mneja el estado de visibilidad de la ventana emergente
 
   return (
     <footer className="footer">
