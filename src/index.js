2fc52e0812a1fff17e792a71d582c97640db9e20
Jose <jause6@duck.com>
2024-12-18 07:45:56 +0100

    DEPURACIÓN error event handler en DOM | index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-/* const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-); */
-
 // Forzar todos los event listeners a ser pasivos
 const supportsPassive = (() => {
   let supported = false;
   try {
-    window.addEventListener('test', null, Object.defineProperty({}, 'passive', {
-      get: () => supported = true
-    }));
+    window.addEventListener(
+      'test',
+      null,
+      Object.defineProperty({}, 'passive', {
+        get: () => (supported = true),
+      })
+    );
   } catch (e) {}
   return supported;
 })();
