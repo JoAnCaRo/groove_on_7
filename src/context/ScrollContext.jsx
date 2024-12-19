954b60a78830e71eee31783e1f087fe2c381c7ce
Jose <jause6@duck.com>
2024-12-19 18:20:04 +0100

    Limpieza Tree Files 4

--- src/context/ScrollContext.jsx
+++ src/context/ScrollContext.jsx
@@ -1,8 +1,11 @@
+/* Importa React, hooks y contexto personalizado */
 import React, { createContext, useContext, useRef } from 'react';
 
+/* Crea contexto para manejar el scroll entre secciones */
 const ScrollContext = createContext();
 
 export const ScrollProvider = ({ children }) => {
+  /* Se crean referencias para cada sección y se utilizarán para identificar las posiciones de las secciones en el DOM */
   const sections = {
     about: useRef(null),
     liveSessions: useRef(null),
@@ -11,28 +14,31 @@ export const ScrollProvider = ({ children }) => {
     contact: useRef(null),
   };
 
+  /* Función para desplazar la vista a una sección específica */
   const scrollToSection = (sectionId) => {
     const target = sections[sectionId]?.current;
     const adjustment = 60; // Avanza 10px más
 
     if (target) {
-      // Obtén la altura del encabezado fijo
+      /* Obtiene altura del encabezado fijo. Si no existe, utiliza 0 como valor por defecto */
       const headerHeight = document.querySelector('.navbar')?.offsetHeight || 0;
 
-      // Ajusta la posición objetivo
+      /* Ajusta posición objetivo */
       const targetPosition = target.offsetTop - headerHeight + adjustment;
 
       const duration = 1700;
       const startPosition = window.scrollY;
       let startTime = null;
 
+      /* Función para suavizar el desplazamiento (ease-in-out) */
       const easeInOutQuad = (t, b, c, d) => {
         t /= d / 2;
-        if (t < 1) return (c / 2) * t * t + b;
+        if (t < 1) return (c / 2) * t * t + b; // Acelera
         t--;
-        return (-c / 2) * (t * (t - 2) - 1) + b;
+        return (-c / 2) * (t * (t - 2) - 1) + b; // Desacelera
       };
 
+      /* Función para la animación cuadro a cuadro */
       const animation = (currentTime) => {
         if (!startTime) startTime = currentTime;
         const timeElapsed = currentTime - startTime;
